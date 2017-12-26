9db24c248f4d924e5b0fed48e947b3423c40d9a4
Jennifercarmen <jennfercarmens@gmail.com>
2017-12-26 14:36:38 -0500

    animation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 jQuery(document).ready(function(){
+
+
+  $( ".logo" ).animate({ "left": "+=28%" }, "slow" );
+ $('h4').animate({'font-size':24, 'border-width':5},1800);
+
   setTimeout(function() {
-    $(".content").fadeOut(1500);
-},3000);
+   $(location).attr('href','main.html'); 
+},2000);
   })
