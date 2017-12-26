1a9f06166d3238a249d53760b695c9faeb017754
Jennifercarmen <jennfercarmens@gmail.com>
2017-12-26 16:43:30 -0500

    data and modal

---
+++ js/data.js
@@ -0,0 +1,18 @@
+var data = {
+      'Cevicheria': [
+        {
+          'name': 'Entre Patas',
+          'Adress': 'Av. Gullman 121',
+          'Prices': '18.00-160.00'
+           'Otros': 'Servicio a Domicilio'
+}
+
+        {
+          'name': 'Mar de Sabores',
+          'Adress': 'Av. Gullman 121',
+          'Prices': '18.00-160.00'
+           'Otros': 'Servicio a Domicilio'
+}
+          ]
+        };
+        
