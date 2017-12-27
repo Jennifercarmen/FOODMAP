c5eb8fe256b912ec8e654c70541fa0cc05b089cd
Jennifercarmen <jennfercarmens@gmail.com>
2017-12-27 18:02:41 -0500

    actualizando data

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
-jQuery(document).ready(function(){
-
-
-  $( ".logo" ).animate({ "left": "+=28%" }, "slow" );
- $('h4').animate({'font-size':24, 'border-width':5},1800);
-
-  setTimeout(function() {
-   $(location).attr('href','main.html'); 
-},2000);
-  })
+jQuery(document).ready(function () {
+	$(".logo").animate({ "left": "+=28%" }, "slow");
+	$('h4').animate({ 'font-size': 24, 'border-width': 5 }, 1800);
+	$( ".main" ).hide();
+	setTimeout(function () {
+		$( ".splash" ).hide();
+		$( ".main" ).show();
+	}, 2000);
+})
