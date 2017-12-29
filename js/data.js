6ec839465887646f3f92ea7ad1cf685ce28c337e
Jennifercarmen <jennfercarmens@gmail.com>
2017-12-28 23:51:48 -0500

    Busqueda sensitiva

--- js/data.js
+++ js/data.js
@@ -1,6 +1,7 @@
 var data = {
   'Picanterias': [
     {
+      'id': 'P1',
       'name': 'Entre Patas',
       'Adress': 'Av. Gullman 121',
       'Prices': '$$',
@@ -11,6 +12,7 @@ var data = {
     },
 
     {
+      'id': 'P2',
       'name': 'Bocano',
       'Adress': 'Av. Grau 152',
       'Prices': '$$',
@@ -24,6 +26,7 @@ var data = {
     },
 
     {
+      'id': 'P3',
       'name': 'SubMarino',
       'Adress': 'Av.Benavides 1080',
       'Prices': '$$$',
@@ -36,6 +39,7 @@ var data = {
     },
 
     {
+      'id': 'P4',
       'name': 'Las Meches',
       'Adress': 'Calle Piura 2012 SJM',
       'Prices': '$$$',
@@ -48,6 +52,7 @@ var data = {
     },
 
     {
+      'id': 'P5',
       'name': 'La Marcación',
       'Adress': 'Av.Alfonso hugarte 1080',
       'Prices': '$$$',
@@ -57,9 +62,21 @@ var data = {
       'image': 'assets/images/Picanterias/Marcación.jpg'
 
     },
+    {
+      'id': 'P6',
+      'name': 'Las Dos Jarras',
+      'Adress': 'Av. Sanchez Cerro Mza. Z Lote. 1',
+      'Prices': '$$$',
+      'Descripcion': 'Ambiente relajado · Agradable',
+      'Telefono': '987456258',
+      'Apertura': 'Abre a las 18:00',
+      'image': 'assets/images/Picanterias/Jarras.jpg'
+
+    },
   ],
   'Pollerias': [
     {
+      'id': 'Pol1',
       'name': 'Cali',
       'Adress': 'Av. Lama 121',
       'Prices': '$',
@@ -72,6 +89,7 @@ var data = {
     },
 
     {
+      'id': 'Pol2',
       'name': 'Norkys',
       'Adress': ' Av Guardia Civil',
       'Prices': '$$$',
@@ -83,6 +101,7 @@ var data = {
 
     },
     {
+      'id': 'Pol3',
       'name': 'Puerto Madero',
       'Adress': 'Surco 126',
       'Prices': '$$$',
@@ -94,6 +113,7 @@ var data = {
 
     },
     {
+      'id': 'Pol4',
       'name': 'Pollos y Parrilladas El Leñador',
       'Adress': 'Huancavelica 120',
       'Prices': '$$$',
@@ -105,6 +125,7 @@ var data = {
 
     },
     {
+      'id': 'Pol5',
       'name': 'Brasa Roja Chicken',
       'Adress': 'Av. Sta. Victoria 700',
       'Prices': '$$$',
@@ -115,10 +136,24 @@ var data = {
       'image': 'assets/images/Pollerias/BRoja.jpg'
 
     },
+    {
+      'id': 'Pol6',
+      'name': 'Pollo Dorado',
+      'Adress': 'Miguel Grau 201',
+      'Prices': '$$$',
+      'Descripcion': 'Ambiente relajado · Agradable',
+      'Telefono': '(01) 2355848',
+      'Apertura': 'Abre a las 18:00',
+
+      'image': 'assets/images/Pollerias/PolloDorado.jpg'
+
+    },
+    
   ],
 
   'Buffet': [
     {
+      'id': 'Buf1',
       'name': 'Mil Sabores',
       'Adress': 'Santa Margarita calle 9, Mz EJ Lt 20',
       'Prices': '$$',
@@ -130,6 +165,7 @@ var data = {
     },
 
     {
+      'id': 'Buf2',
       'name': 'Saiyajin Buffet',
       'Adress': 'Santa Margarita 258',
       'Prices': '$$',
@@ -139,6 +175,7 @@ var data = {
       'image': 'assets/images/Buffet/Saiyajin.jpg'
     },
     {
+      'id': 'Buf3',
       'name': 'Don Parce',
       'Adress': 'Av. Sanchez Cerro 125',
       'Prices': '$$',
@@ -148,6 +185,7 @@ var data = {
       'image': 'assets/images/Buffet/Parce.png'
     },
     {
+      'id': 'Buf4',
       'name': 'Chilis',
       'Adress': ', Av. Andrés Avelino Cáceres 147',
       'Prices': '$$',
@@ -157,6 +195,7 @@ var data = {
       'image': 'assets/images/Buffet/Chilis.jpg'
     },
     {
+      'id': 'Buf5',
       'name': 'Stampa',
       'Adress': 'Jr. San Martín 535',
       'Prices': '$$',
@@ -165,57 +204,86 @@ var data = {
       'Apertura': 'Abre a las 18:00',
       'image': 'assets/images/Buffet/Stampa.jpg'
     },
+    {
+      'id': 'Buf6',
+      'name': 'Señor Perú',
+      'Adress': 'Mz. V Lt. 4B, Zona Industrial II, Calle 6',
+      'Prices': '$$$',
+      'Descripcion': 'Ambiente relajado · Agradable',
+      'Telefono': '(01) 4587458',
+      'Apertura': 'Abre a las 18:00',
+      'image': 'assets/images/Buffet/Peru.jpg'
+
+    },
   ],
   'Chifa': [
     {
+      'id': 'chif1',
       'name': 'Hao Swao',
       'Adress': 'Av. Benavides 245',
       'Prices': '$$',
       'Descripcion': 'Ambiente relajado · Agradable',
       'Telefono': '945506356',
       'Apertura': 'Abre a las 18:00',
-      'image': 'assets/images/Chifa/Swao.jpg'
+      'image': 'assets/images/chifas/Swao.jpg'
     },
 
     {
+      'id': 'chif2',
       'name': 'Wing Lung',
       'Adress': 'Av Sánchez Cerro 1238',
       'Prices': '$$',
       'Descripcion': 'Ambiente relajado · Agradable',
       'Telefono': '945506356',
       'Apertura': 'Abre a las 18:00',
-      'image': 'assets/images/Chifa/Lung.jpg'
+      'image': 'assets/images/chifas/Lung.jpg'
     },
     {
+      'id': 'chif3',
       'name': 'Taypa',
       'Adress': 'Av.Loreto 652',
       'Prices': '$$',
       'Descripcion': 'Ambiente relajado · Agradable',
       'Telefono': '945506356',
       'Apertura': 'Abre a las 18:00',
-      'image': 'assets/images/Chifa/Taypa.jpg'
+      'image': 'assets/images/chifas/Taypa.jpg'
     },
     {
+      'id': 'chif4',
       'name': 'Wa Chong',
       'Adress': 'Av 28 de julio 238',
       'Prices': '$$',
       'Descripcion': 'Ambiente relajado · Agradable',
       'Telefono': '945506356',
       'Apertura': 'Abre a las 18:00',
-      'image': 'assets/images/Chifa/Chong.jpg'
+      'image': 'assets/images/chifas/Chong.jpg'
     },
     {
+      'id': 'chif5',
       'name': 'Kam Ming',
       'Adress': 'Libertad 377-A',
       'Prices': '$$',
       'Descripcion': 'Ambiente relajado · Agradable',
       'Telefono': '945506356',
       'Apertura': 'Abre a las 18:00',
-      'image': 'assets/images/Chifa/Ming.jpg'
+      'image': 'assets/images/chifas/Ming.jpg'
+    },
+    {
+      'id': 'chif6',
+      'name': 'Casa China',
+      'Adress': 'Jirón Ucayali 214, Cercado de Lima 15001',
+      'Prices': '$$$',
+      'Descripcion': 'Ambiente relajado · Agradable',
+      'Telefono': '(01) 2355848',
+      'Apertura': 'Abre a las 18:00',
+
+      'image': 'assets/images/chifas/CasaChina.jpg'
+
     },
   ],
   'comida rápida': [
     {
+      'id': 'cr1',
       'name': 'Carbón Burger',
       'Adress': 'Jirón Arequipa 788',
       'Prices': '$$',
@@ -226,6 +294,7 @@ var data = {
     },
 
     {
+      'id': 'cr2',
       'name': 'Papa Johns Pizza',
       'Adress': 'Av Sánchez Cerro 1450',
       'Prices': '$$',
@@ -235,6 +304,7 @@ var data = {
       'image': 'assets/images/comida_rapida/Pizza.jpg'
     },
     {
+      'id': 'cr3',
       'name': 'Kentucky Fried Chicken',
       'Adress': 'Mall del sur SJM',
       'Prices': '$$',
@@ -244,6 +314,7 @@ var data = {
       'image': 'assets/images/comida_rapida/Kentucky.jpg'
     },
     {
+      'id': 'cr4',
       'name': 'Burger King',
       'Adress': ' Av. Guardia Civil MZ. Q-18',
       'Prices': '$$',
@@ -253,6 +324,7 @@ var data = {
       'image': 'assets/images/comida_rapida/BurgerKing.jpg'
     },
     {
+      'id': 'cr5',
       'name': 'DGusta Delivery',
       'Adress': 'Av. 28 de Julio 321',
       'Prices': '$$',
@@ -261,7 +333,20 @@ var data = {
       'Apertura': 'Abre a las 18:00',
       'image': 'assets/images/comida_rapida/DGusta.jpg'
     },
+    {
+      'id': 'cr6',
+      'name': 'Sanguchón Campesino',
+      'Adress': 'Viña Picasso 342, Cercado de Lima LIMA 33',
+      'Prices': '$$$',
+      'Descripcion': 'Ambiente relajado · Agradable',
+      'Telefono': '(01) 2355848',
+      'Apertura': 'Abre a las 18:00',
+
+      'image': 'assets/images/comida_rapida/Sanguchon.jpg'
+
+    },
   ],
+  
 
 };
 
